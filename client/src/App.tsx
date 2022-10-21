b09384f9f19493e1f92cc6356574380ae967009c
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-21 08:59:49 -0300

    Switch fetching method to react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
+import { useQuery } from 'react-query'
 
 function App() {
-  const [apiData, setApiData] = useState<ApiResult | undefined>(undefined)
-
-  interface ApiData {
+  interface Livros {
     nome: string,
     autor: string,
     distribuidora: string,
   }
-
   interface ApiResult {
-    message: ApiData[]
+    message: Livros[]
   }
 
-  React.useEffect(() => {
-    fetch("http://127.0.0.1:3000/livros").then(res => res.json()).then(json => setApiData(json))
-  }, [])
+  const { data, isLoading, isError } = useQuery<ApiResult>("livros", () => fetch("http://127.0.0.1:3000/livros").then(res => res.json()))
 
-  if (apiData == undefined) return <div>teste</div>
+  if (isLoading || data == undefined) return <div>Carregando</div>
+
+  if (isError) return <div>Deu erro!!</div>
 
   return (
     <div className="App">
-      {apiData.message.map(livro => (
-        <div>
-          <p>{livro.nome}</p>
-          <p>{livro.autor}</p>
-          <p>{livro.distribuidora}</p>
-        </div>
-      ))}
+      {data.message.map(livro => livro.distribuidora)}
     </div>
   )
 }
